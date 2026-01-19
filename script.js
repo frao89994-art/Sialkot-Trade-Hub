61094965441732b2978d709d8ab2bc1a11b4a3cf
frao89994-art <frao89994@gmail.com>
2026-01-19 14:01:32 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
+function login(){
+  let role=document.getElementById("role").value;
+  if(role==="factory"){
+    window.location="factory-dashboard.html";
+  }else{
+    window.location="dashboard.html";
+  }
+}
+
+function placeOrder(){
+  let name=document.getElementById("name").value;
+  let product=document.getElementById("product").value;
+  let qty=document.getElementById("qty").value;
+  let country=document.getElementById("country").value;
+
+  let commission = country==="Pakistan" ? qty*50 : qty*100;
+
+  let msg=`ORDER | Sialkot Trade Hub
+Buyer: ${name}
+Product: ${product}
+Quantity: ${qty}
+Country: ${country}
+Commission: Rs ${commission}`;
+
+  window.open(
+    "https://wa.me/923042249321?text="+encodeURIComponent(msg),
+    "_blank"
+  );
+}
 
